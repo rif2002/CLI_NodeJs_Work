593da642f8544aa1f6f431c234a7c89ce8be38e1
rif2002 <147876762+rif2002@users.noreply.github.com>
2025-10-16 00:03:27 +0400

    Added Project #2: CLI File Creation App

---
+++ filecreater.js
@@ -0,0 +1,23 @@
+import readline from 'readline';
+import fs from 'fs';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+}); 
+
+const fileCreation = () => {
+    rl.question("Enter your file name: ", (fileName) => {
+        rl.question("Enter your file content: ", (content) => {
+            fs.writeFile(`${fileName}.txt`, content, (err) => {
+                if (err) {
+                    console.error(`Error writing file: ${err.message}`);
+                } else {
+                    console.log(`File "${fileName}.txt" created successfully.`);
+                }
+                rl.close();
+            });
+        });
+    });
+};
+
+fileCreation();
